87759ecf7468bea49dd2600821ca204bc9d4e2da
tweetliable <tweetliable@gmail.com>
2021-04-22 18:14:07 +0630

    Change tweet model

--- src/tweet.ts
+++ src/tweet.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const tweetSchema = new Schema({
-  tweet_id: String,
-  text: String,
-});
+const tweetSchema = new Schema(
+  {
+    tweet_id: String,
+    text: String,
+  },
+  { timestamps: true }
+);
 
 export const Tweet = mongoose.model("Tweet", tweetSchema);
